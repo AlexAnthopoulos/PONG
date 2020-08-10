9359e725b9eb1c555247fef66110e30158078fc2
Kochasa <67097121+Kochasa@users.noreply.github.com>
2020-08-10 19:08:08 +0200

    add Pong class

--- pong.js
+++ pong.js
@@ -41,49 +41,57 @@ class Ball extends Rect
         this.vel = new Vec
     }
 }
+
+class Pong  
+{
+    constructor(canvas)
+    {
+        this._canvas = canvas;
+        this._ctx = canvas.getContext('2d');
+        this.ball = new Ball;
+        console.log(ball);
+        this.ball.pos.x = 100;
+        this.ball.pos.y = 50
+        this.ball.vel.x = 900;
+        this.ball.vel.y = 800;
+        //Creating animation update for the page
+        let lastUpdate;
+
+        function callback(milliseconds) {
+            if(lastUpdate) {
+                //Update in seconds for easier changes
+                update((milliseconds - lastUpdate)/1000);
+            }
+            //Doing this so we add on the animation frame the callback
+            lastUpdate = milliseconds;
+            requestAnimationFrame(callback);
+        }
+            callback();
+    }
+    update(dt){
+        //Movement of the ball relative to time difference
+        this.ball.pos.x += this.ball.vel.x * dt;
+        this.ball.pos.y += this.ball.vel.y * dt;
+        
+        //Create bouncing of the ball by inverting the velocity on both x and y axis across the corners of the board
+        if(this.ball.left < 0 || this.ball.right > this._canvas.width){
+            this.ball.vel.x = -this.ball.vel.x;
+        }
+        if(this.ball.top < 0 || this.ball.bottom > this._canvas.height){
+            this.ball.vel.y = -this.ball.vel.y;
+        }
+        //Create board stuff
+        this._ctx.fillStyle = '#000';
+        this._ctx.fillRect(0,0,this._canvas.width,this._canvas.height);
+        //Create the ball
+        this._ctx.fillStyle = '#fff';
+        this._ctx.fillRect(this.ball.pos.x, this.ball.pos.y, this.ball.size.x, this.ball.size.y);
+    }
+}
 //Draw main gaming area and get canvas elements
 const canvas = document.getElementById('pong');
-const ctx = canvas.getContext('2d');
+const pong = new Pong(canvas);
 
 
-const ball = new Ball;
-console.log(ball);
-ball.pos.x = 100;
-ball.pos.y = 50
-ball.vel.x = 900;
-ball.vel.y = 800;
-//Creating animation update for the page
-let lastUpdate;
 
-function callback(milliseconds) {
-    if(lastUpdate) {
-        //Update in seconds for easier changes
-        update((milliseconds - lastUpdate)/1000);
-    }
-    //Doing this so we add on the animation frame the callback
-    lastUpdate = milliseconds;
-    requestAnimationFrame(callback);
-}
 
-//Updating ball position
-function update(dt){
-    //Movement of the ball relative to time difference
-    ball.pos.x += ball.vel.x * dt;
-    ball.pos.y += ball.vel.y * dt;
-    
-    //Create bouncing of the ball by inverting the velocity on both x and y axis across the corners of the board
-    if(ball.left < 0 || ball.right > canvas.width){
-        ball.vel.x = -ball.vel.x;
-    }
-    if(ball.top < 0 || ball.bottom > canvas.height){
-        ball.vel.y = -ball.vel.y;
-    }
-    //Create board stuff
-    ctx.fillStyle = '#000';
-    ctx.fillRect(0,0,canvas.width,canvas.height);
-    //Create the ball
-    ctx.fillStyle = '#fff';
-    ctx.fillRect(ball.pos.x, ball.pos.y, ball.size.x, ball.size.y);
-}
-
-callback();
