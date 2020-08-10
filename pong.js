cd51c4b724cc5bb4d9b4a8c3bb833a7438c46290
Kochasa <67097121+Kochasa@users.noreply.github.com>
2020-08-10 17:56:35 +0200

    added ball

--- pong.js
+++ pong.js
@@ -1,4 +1,9 @@
+//Draw main gaming area and get canvas elements
 const canvas = document.getElementById('pong');
 const ctx = canvas.getContext('2d');
 ctx.fillStyle = '#000';
 ctx.fillRect(0,0,canvas.width,canvas.height);
+
+//Create the ball
+ctx.fillStyle = '#fff';
+ctx.fillRect(0,0,10,10);
