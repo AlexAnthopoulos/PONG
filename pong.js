c89ce46873a7c8f880e978569a949a964a400e96
Kochasa <67097121+Kochasa@users.noreply.github.com>
2020-08-10 18:04:00 +0200

    add ball class

--- pong.js
+++ pong.js
@@ -16,12 +16,22 @@ class Rect
     }
 }
 
+class Ball extends Rect
+{
+    constructor()
+    {
+        super(10,10);
+        this.vel = new Vec
+    }
+}
 //Draw main gaming area and get canvas elements
 const canvas = document.getElementById('pong');
 const ctx = canvas.getContext('2d');
 ctx.fillStyle = '#000';
 ctx.fillRect(0,0,canvas.width,canvas.height);
 
+const ball = new Ball;
+console.log(ball);
 //Create the ball
 ctx.fillStyle = '#fff';
 ctx.fillRect(0,0,10,10);
