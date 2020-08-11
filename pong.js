445d483f251813093a2611094bf3b53824be90ee
Kochasa <67097121+Kochasa@users.noreply.github.com>
2020-08-11 15:40:18 +0200

    add image and start paddle creation

--- pong.js
+++ pong.js
@@ -1,7 +1,7 @@
 let game = new Phaser.Game(1200,800,Phaser.AUTO,``,{preload : preload, create: create, update: update});
 
 function preload(){
-
+    game.load.image('paddle','assets/paddle.png')
 }
 
 function create(){
@@ -9,5 +9,14 @@ function create(){
 }
 
 function update(){
-    
+
+}
+
+
+function create_paddle(x,y){
+    let paddle = game.add.sprite(x,y,`paddle`);
+    paddle.anchor.setTo(0.5,0.5);
+    game.physics.arcade.enable(paddle);
+    paddle.body.collideWorldBounds = true;
+    return paddle;
 }
