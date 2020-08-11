758a9b5e1ae7259e531f1b39671ca2855854ae5a
Kochasa <67097121+Kochasa@users.noreply.github.com>
2020-08-11 15:32:19 +0200

    add phaser and preload stuff

---
+++ pong.js
@@ -0,0 +1,13 @@
+let game = new Phaser.Game(1200,800,Phaser.AUTO,``,{preload : preload, create: create, update: update});
+
+function preload(){
+
+}
+
+function create(){
+
+}
+
+function update(){
+    
+}
