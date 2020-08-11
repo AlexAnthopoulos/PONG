6e8bb14399f58f8a1e2a0d7d13e394c29d9b3b83
Kochasa <67097121+Kochasa@users.noreply.github.com>
2020-08-11 16:39:38 +0200

    both paddles are inside the board

--- js/pong.js
+++ js/pong.js
@@ -2,11 +2,7 @@ const config = {
     type:Phaser.AUTO,
     parent: `game`,
     width: 1200,
-    height :800,
-    scale: {
-        mode:Phaser.Scale.RESIZE,
-        autoCenter: Phaser.Scale.Center_BOTH
-    },
+    height :800,    
     scene: {
         preload,
         create,
@@ -22,6 +18,7 @@ const config = {
 
 const game = new Phaser.Game(config);
 let ball;
+let player1;
 
 function preload() {
     this.load.image(`ball`,`../assets/ball.png`);
@@ -33,6 +30,16 @@ function create(){
         this.physics.world.bounds.width / 2,
         this.physics.world.bounds.height / 2,
         'ball'
+    )
+    player1 = this.physics.add.sprite(
+        this.physics.world.bounds.width - (ball.body.width / 2 + 1),
+        this.physics.world.bounds.height / 2,
+        'paddle'
+    )
+    player2 = this.physics.add.sprite(
+        ball.body.width / 2 + 1,
+        this.physics.world.bounds.height / 2,
+        'paddle'
     )
 }
 
