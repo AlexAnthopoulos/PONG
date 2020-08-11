598efce4bba4bee3ebfee31b72fe73197ea68eee
Kochasa <67097121+Kochasa@users.noreply.github.com>
2020-08-11 15:47:06 +0200

    add paddle control function

--- pong.js
+++ pong.js
@@ -1,11 +1,15 @@
 let game = new Phaser.Game(1200,800,Phaser.AUTO,``,{preload : preload, create: create, update: update});
 
+let paddle1;
+let paddle2;
+
 function preload(){
     game.load.image('paddle','assets/paddle.png')
 }
 
 function create(){
-
+    paddle1 = create_paddle(0,game.world.centerY)
+    paddle2 = create_paddle(game.world.width - 16, game.world.centerY)
 }
 
 function update(){
@@ -19,4 +23,13 @@ function create_paddle(x,y){
     game.physics.arcade.enable(paddle);
     paddle.body.collideWorldBounds = true;
     return paddle;
+}
+
+function control_paddle(paddle,y){
+    paddle.y = y;
+    if(paddle.y < paddle.height / 2){
+        paddle.y = paddle.height / 2;
+    } else if (paddle.y > game.world.height - paddle.height / 2 ){
+        paddle.y = game.world.height - paddle.height / 2;
+    }
 }
