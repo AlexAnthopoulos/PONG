5f665f6268e2bc50d2ec7f5655f39c3400ba6702
Kochasa <67097121+Kochasa@users.noreply.github.com>
2020-08-11 16:32:16 +0200

    add ball in the game

--- js/pong.js
+++ js/pong.js
@@ -1,5 +1,3 @@
-
-
 const config = {
     type:Phaser.AUTO,
     parent: `game`,
@@ -8,6 +6,11 @@ const config = {
     scale: {
         mode:Phaser.Scale.RESIZE,
         autoCenter: Phaser.Scale.Center_BOTH
+    },
+    scene: {
+        preload,
+        create,
+        update
     },
     physics: {
         default: 'arcade',
@@ -18,3 +21,21 @@ const config = {
 }
 
 const game = new Phaser.Game(config);
+let ball;
+
+function preload() {
+    this.load.image(`ball`,`../assets/ball.png`);
+    this.load.image(`paddle`,`../assets/paddle.png`);
+}
+
+function create(){
+    ball = this.physics.add.sprite(
+        this.physics.world.bounds.width / 2,
+        this.physics.world.bounds.height / 2,
+        'ball'
+    )
+}
+
+function update(){
+
+}
